a65711d26bfb2b0dd481580999dfb8b346110afc
Keana Cobarde <keanacobarde@gmail.com>
2023-10-04 21:47:52 -0500

    getting initial setup complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <h1>Hey, React!</h1> <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2> Pizza </h2>
+      {pizzaData.map((pizza) => <h3 key={pizza.name}> {pizza.name} </h3>)}
+    </div>
+  );
+}
+
+//REACT v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
